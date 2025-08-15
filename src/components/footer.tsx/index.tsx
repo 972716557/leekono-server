7a022a316e761cb98ef90831843b1e1cfaa6b9c2
虞晨 <chen.yu@cicc.com.cn>
2025-08-15 17:01:33 +0800

    init

---
+++ src/components/footer.tsx/index.tsx
@@ -0,0 +1,25 @@
+"use se";
+const LeekonoFooter = () => {
+  return (
+    <footer className="flex justify-center align-center p-[32px_10px_20px] text-[12px] text-[#999]">
+      <div>
+        {/* <FormattedMessage id="phoneLabel" />:
+        <span className={`${prefix}-footer-blue`}>
+          <FormattedMessage id="phone" />
+        </span> */}
+      </div>
+      <div>{/* <FormattedMessage id="email" /> */}</div>
+      <div>Leekono © 2025</div>
+      {/* <div>
+        <a href="/#/business-license">
+          <FormattedMessage id="businessLicense" />
+        </a>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <FormattedMessage id="address" />
+      </div> */}
+    </footer>
+  );
+};
+
+export default LeekonoFooter;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useState } from "react";
+import { Popup } from "antd-mobile";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { GlobalOutlined, MenuOutlined } from "@ant-design/icons";
+import logo from "../../../public/favicon-title.png";
+
+const prefix = "leekono-layout-mobile-header";
+const MobileHeader = () => {
+  const router = useRouter();
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+
+  const onTouchEnd = (path: string) => {
+    router.push(path);
+    setVisible(false);
+  };
+
+  const onClickHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <header className="top-[0] left-[0] right-[0] pd-20 w-full fixed z-100001 shadow-[0_8px_24px_-2px_rgba(0,0,0,0.05)]">
+      <div className="relative flex items-center justify-center">
+        <span onTouchEnd={onClick} className="absolute left-[20px] font-[16px]">
+          <MenuOutlined />
+        </span>
+        <span onClick={onClickHome}>
+          <Image src={logo} alt="logo" className="w-[166px]" />
+        </span>
+        <span className="absolute right-[20px] font-[16px]">
+          <GlobalOutlined />
+        </span>
+      </div>
+      <Popup
+        visible={visible}
+        onMaskClick={() => {
+          setVisible(false);
+        }}
+        onClose={() => {
+          setVisible(false);
+        }}
+        position="left"
+        maskClassName={`${prefix}-mask`}
+        bodyStyle={{
+          width: "60vw",
+          height: "calc(100vh - 50px)",
+          top: 50,
+          background: "#fff",
+          borderRight: "1px solid #ebedf1",
+        }}
+      >
+        {/* {routes.map((item) => (
+          <div
+            key={item.id}
+            onTouchEnd={() => onTouchEnd(item.path)}
+            className={`${prefix}-side`}
+          >
+            <div className={`${prefix}-side-item`}></div>
+          </div>
+        ))} */}
+      </Popup>
+    </header>
+  );
+};
+export default MobileHeader;
