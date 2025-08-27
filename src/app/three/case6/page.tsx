ceca1426501e4dd6c2046d2489dddebc5ccdf368
虞晨 <chen.yu@cicc.com.cn>
2025-08-27 18:26:41 +0800

    增加小球发射demo

--- src/app/three/case6/page.tsx
+++ src/app/three/case6/page.tsx
@@ -9,6 +9,10 @@ import {
   MeshBasicMaterial,
   Mesh,
   PlaneGeometry,
+  TextureLoader,
+  Vector2,
+  Raycaster,
+  Vector3,
 } from "three";
 import {
   World,
@@ -31,14 +35,15 @@ const Page = () => {
       0.1, // 相机最近能看到的是什么
       1000 // 相机最远能看到什么
     );
-    camera.position.z = 8;
+    camera.position.z = 30;
     camera.position.x = 1;
     camera.position.y = 1;
     const renderer = new WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     const controller = new OrbitControls(camera, renderer.domElement);
     const material = new MeshBasicMaterial({
-      color: 0x00ff00,
+      // color: 0x00ff00,
+      map: new TextureLoader().load("http://localhost:3000/light.png"),
     });
 
     // 创建物理事件
@@ -59,7 +64,7 @@ const Page = () => {
 
     // 创建物理地面
     const ground = new Body({
-      // 这表示这个物体是固定的
+      // 质量设置为0，这表示这个物体是固定的
       mass: 0,
       shape: new Plane(),
       material: groundMaterial,
@@ -67,44 +72,76 @@ const Page = () => {
 
     world.addBody(ground);
     ground.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    const spheres = [];
+    const createSphere = (position, direction) => {
+      // 创建可视化球体
+      const sphere = new Mesh(new SphereGeometry(1, 32, 32), material);
+      sphere.position.copy(position);
+      // 创建物理球体
+      const sphereBody = new Body({
+        // 设置质量为1
+        mass: 1,
+        position: sphere.position as unknown as Vec3,
+        material: sphereMaterial,
+        // 设置阻力，不会一直往前跑
+        linearDamping: 0.5,
+        angularDamping: 1,
+      });
+      const sphereShape = new Sphere(1);
+      sphereBody.addShape(sphereShape);
 
-    // 创建可视化球体
-    const sphere = new Mesh(new SphereGeometry(1, 32, 32), material);
-    sphere.position.y = 5;
+      // 给物理球体增加水平方向的力，这个数值不仅仅表示方向，而且还代表力度，数值越大，球体就会飞得越快、越远
+      sphereBody.applyLocalForce(direction.scale(2000), new Vec3(0, 0, 0));
+      world.addBody(sphereBody);
 
-    // 创建物理球体
-    const sphereBody = new Body({
-      mass: 1,
-      position: sphere.position,
-      material: sphereMaterial,
-    });
-    const sphereShape = new Sphere(1);
-    sphereBody.addShape(sphereShape);
-    world.addBody(sphereBody);
+      scene.add(sphere);
+      sphere.position.copy(sphereBody.position);
+      spheres.push({ sphere, sphereBody });
+    };
 
-    scene.add(sphere);
+    // 创建可视化地面
     const plane = new Mesh(
-      new PlaneGeometry(10, 10),
+      new PlaneGeometry(30, 30),
       new MeshBasicMaterial({
         color: 0xffffff,
       })
     );
 
     plane.rotation.x = -Math.PI / 2;
+
     scene.add(plane);
     const updatePhysics = () => {
       // 每一秒60帧
       world.step(1 / 60);
       // 把物理小球的位置复制到可视化小球
-      sphere.position.copy(sphereBody.position);
     };
     function animate() {
       updatePhysics();
       controller.update();
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
+      spheres.forEach(({ sphere, sphereBody }) => {
+        sphere.position.copy(sphereBody.position);
+        sphere.quaternion.copy(sphereBody.quaternion);
+      });
     }
     animate();
+    const mouse = new Vector2();
+
+    window.addEventListener("click", (e) => {
+      mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+      const raycaster = new Raycaster();
+      raycaster.setFromCamera(mouse, camera);
+      const pos = new Vector3();
+      pos.copy(raycaster.ray.direction).add(raycaster.ray.origin);
+      const direction = new Vec3(
+        raycaster.ray.direction.x,
+        raycaster.ray.direction.y,
+        raycaster.ray.direction.z
+      );
+      createSphere({ x: pos.x, y: pos.y, z: pos.z }, direction);
+    });
     ref.current?.append(renderer.domElement);
   }, []);
   return (
