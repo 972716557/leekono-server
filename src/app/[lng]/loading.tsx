c67c148a8b45023679184f1eb97a0398ae29642a
yuchen <chen.yu@thunderbit.com>
2026-02-26 12:23:09 +0800

    优化loading页面效果

--- src/app/[lng]/loading.tsx
+++ src/app/[lng]/loading.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Loading: React.FC = () => (
   <div className="leekono-loading">
-    <div className="leekono-loading-dots">
+    <div className="leekono-loading-ring">
+      <span className="leekono-loading-dot" />
+      <span className="leekono-loading-dot" />
+      <span className="leekono-loading-dot" />
       <span className="leekono-loading-dot" />
       <span className="leekono-loading-dot" />
       <span className="leekono-loading-dot" />
